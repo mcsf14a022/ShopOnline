0c50a54cc4888d7ea77e0d5dba091c14a32b56b8
faryaljafar <faryaljafar1993@gmail.com>
2016-02-02 10:30:10 +0500

    Latest Commit

---
+++ js/map.js
@@ -0,0 +1,11 @@
+﻿function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: new google.maps.LatLng(31.54972, 74.34361),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+google.maps.event.addDomListener(window, 'load', initialize);
